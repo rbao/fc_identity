f5d4ce43bbbf6fbe36d9f3845d7a8ca4f8ea5d62
Roy Bao <roy@roybao.com>
2018-10-07 21:42:42 -0700

    Remove generate_ids function

--- lib/fc_identity/support.ex
+++ lib/fc_identity/support.ex
@@ -7,20 +7,4 @@ defmodule FCIdentity.Support do
     filtered_src = Map.take(src, keys)
     struct(dest, filtered_src)
   end
-
-  # Generate uuid for keys of struct ends in `id_`
-  def generate_ids(struct, keys \\ nil) do
-    keys = keys || Enum.filter(Map.keys(struct), fn(key) ->
-      sk = Atom.to_string(key)
-      String.ends_with?(sk, "id")
-    end)
-
-    Enum.reduce(keys, struct, fn(k, acc) ->
-      if is_nil(Map.get(struct, k)) do
-        Map.put(acc, k, UUID.uuid4())
-      else
-        acc
-      end
-    end)
-  end
 end
